a696741ec77c42dc4c9ea0771ca8300d1dceb8fb
Stephanie Hicks <stepallday@yahoo.com>
2021-02-02 21:49:05 -0500

    aspnet started

---
+++ Testing/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testing.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int CateoryID { get; set; }
+        public int OnSale { get; set; }
+        public int StockLevel { get; set; }
+
+
+    }
+}
---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    public class ProductRepository
+    {
+        private readonly IDbConnection _conn;
+            public ProductRepository
+    }
+}
